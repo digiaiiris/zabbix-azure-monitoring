31fd1a2a9d163efb4fc2c0648b77558a7798f16c
Antti-Pekka Meronen <antti-pekka.meronen@digia.com>
2019-05-27 08:51:42 +0000

    Interval based on timegrain parameter.

--- python/ic_azure/insights_metric.py
+++ python/ic_azure/insights_metric.py
@@ -3,6 +3,7 @@
 # Python imports
 from datetime import datetime, timedelta
 from argparse import ArgumentParser
+import re
 
 # Azure client imports
 from azure_client import AzureClient
@@ -16,15 +17,37 @@ class InsightsMetric(object):
         self.subscription_id = azure_client.subscription_id
 
     # Method to retrieve metrics from Microsoft Insight resources
-    def get_metric(self, resource_group, resource_name, interval, metric,
-                   statistic, timegrain, timeshift):
+    def get_metric(self, resource_group, resource_name, metric, statistic,
+                   timegrain, timeshift):
 
         # Declare variables
+        interval = -1
         ret_val = -1
 
-        # Calculate timestamps
+        # Retrieve interval and timeunit
+        result = re.search(r"^PT?([\d]+)([DHM])$", timegrain)
+
+        # Check the result object
+        if not result:
+            raise ValueError("Timegrain value is invalid.")
+
+        # Retrieve and check interval
+        interval = int(result.group(1))
+        if interval < 1:
+            raise ValueError("Timegrain interval is not valid.")
+
+        # Calculate end time
         end_time = datetime.utcnow() - timedelta(seconds=timeshift)
-        start_time = end_time - timedelta(seconds=interval)
+
+        # Retrieve time unit and calculate start time
+        if result.group(2) == "D":
+            start_time = end_time - timedelta(days=interval)
+        elif result.group(2) == "H":
+            start_time = end_time - timedelta(hours=interval)
+        elif result.group(2) == "M":
+            start_time = end_time - timedelta(minutes=interval)
+        else:
+            raise ValueError("Unable to retrieve unit from timegrain.")
 
         # Create resource ID
         resource_id = "subscriptions/{}/resourceGroups/{}".format(
@@ -67,11 +90,10 @@ def main(args=None):
     parser.add_argument("--timeshift", type=int, default=0,
                         help="Time shift for interval")
     parser.add_argument("metric", help="Metric to obtain")
-    parser.add_argument("interval", type=int, help="Statistic interval")
     parser.add_argument("statistic", help="Statistic to retrieve. e.g. " +
                         "None, Average, Count, Minimum, Maximum, Total")
     parser.add_argument("timegrain", help="Timegrain for metric. e.g. " +
-                        "None, PT1M, PT1H, P1D")
+                        "PT1M, PT1H, P1D")
 
     args = parser.parse_args(args)
 
@@ -84,7 +106,6 @@ def main(args=None):
     value = client.get_metric(
         args.resource_group,
         args.resource_name,
-        args.interval,
         args.metric,
         args.statistic,
         args.timegrain,
