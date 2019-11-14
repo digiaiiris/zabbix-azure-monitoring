41b59f79526f4e5ad4c1625a67a5c8bff6c391c1
Antti-Pekka Meronen <bulkware@users.noreply.github.com>
2019-11-14 10:58:06 +0200

    Azure Logic Apps discoveries. (#10)

--- python/ic_azure/azure_kusto.py
+++ python/ic_azure/azure_kusto.py
@@ -10,11 +10,11 @@ from azure_client import AzureClient
 
 def main(args=None):
     parser = ArgumentParser(
-        description="Run Kusto-queries to Azure's REST APIs"
+        description="Run Kusto queries to Azure's REST APIs"
     )
 
     parser.add_argument("config", type=str, help="Path to configuration file.")
-    parser.add_argument("query", type=str, help="Kusto-query to run.")
+    parser.add_argument("query", type=str, help="Kusto query to run.")
 
     args = parser.parse_args(args)
 
@@ -25,13 +25,19 @@ def main(args=None):
     # Instantiate Azure Kusto-client
     azure_client = AzureClient(args, api=api, kusto=True)
 
-    # Predefined queries start with a certain string
+    # Match predefined queries
     query = args.query
     if azure_client.kusto_queries.get(query):
         query = azure_client.kusto_queries.get(query)
 
-    # Run Kusto-query and return response
-    response = azure_client.kusto_query(query)
+    # Post query
+    response = azure_client.query(
+        method="POST",
+        json={"query": query},
+        url="{}v1/apps/{}/query".format(api, azure_client.application_id)
+    )
+
+    # Print response
     print(json.dumps(response))
 
 
---
+++ python/ic_azure/azure_logic_apps.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+# Python imports
+import json
+from argparse import ArgumentParser
+
+# Azure client imports
+from azure_client import AzureClient
+
+
+def main(args=None):
+    parser = ArgumentParser(
+        description="Discover workflows from Azure Logic Apps."
+    )
+
+    parser.add_argument("config", type=str, help="Path to configuration file")
+    parser.add_argument("resource_group", type=str,
+                        help="Resource group name.")
+    parser.add_argument("-t", "--trigger", dest="trigger", type=str,
+                        help="Logic App trigger name.")
+    parser.add_argument("-w", "--workflow", dest="workflow", type=str,
+                        help="Logic App workflow name.")
+    parser.add_argument("--version", dest="version", default="2016-06-01",
+                        type=str, help="Azure REST API version.")
+
+    args = parser.parse_args(args)
+
+    # Instantiate Azure client
+    azure_client = AzureClient(args)
+
+    # Generate query base URL
+    url = "https://management.azure.com"
+    url += "/subscriptions/{}".format(azure_client.subscription_id)
+    url += "/resourceGroups/{}".format(args.resource_group)
+    url += "/providers/Microsoft.Logic"
+
+    # Modify query based on arguments
+    if args.trigger:  # Trigger history
+        url += "/workflows/{}".format(args.workflow)
+        url += "/triggers/{}".format(args.trigger)
+        url += "/histories?api-version={}".format(args.version)
+    elif args.workflow:  # Triggers
+        url += "/workflows/{}".format(args.workflow)
+        url += "/triggers?api-version={}".format(args.version)
+    else:  # Workflows
+        url += "/workflows?api-version={}".format(args.version)
+
+    # Run query to API
+    response = azure_client.query(method="GET", url=url)
+
+    # Print results depending on arguments
+    discovery = []
+    for item in response.get("value"):
+        if args.trigger:  # Trigger history
+            discovery.append({
+                "{#HISTORY_ID}": item.get("id"),
+                "{#HISTORY_STATUS}": item.get("properties").get("status"),
+                "{#HISTORY_NAME}": item.get("name")
+            })
+            continue
+
+        # Only include enabled items of triggers and workflows
+        if item.get("properties").get("state") != "Enabled":
+            continue
+
+        if args.workflow:  # Triggers
+            discovery.append({
+                "{#TRIGGER_ID}": item.get("id"),
+                "{#TRIGGER_NAME}": item.get("name")
+            })
+        else:  # Workflows
+            discovery.append({
+                "{#WORKFLOW_ID}": item.get("id"),
+                "{#WORKFLOW_NAME}": item.get("name")
+            })
+
+    # Output discovery
+    discovery = {"data": discovery}
+    print(json.dumps(discovery))
+
+
+if __name__ == "__main__":
+    main()
