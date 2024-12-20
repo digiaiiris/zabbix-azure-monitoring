21adaa896b4cba82f25a79f7b0cdcd8e4c26ee03
Antti-Pekka Meronen <antti-pekka.meronen@digia.com>
2024-12-20 07:34:30 +0200

    Version 1.21

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="azure-monitoring",
-    version="1.20",
+    version="1.21",
     author="Antti-Pekka Meronen",
     author_email="antti-pekka.meronen@digia.com",
     description="Monitoring scripts for Azure services",
